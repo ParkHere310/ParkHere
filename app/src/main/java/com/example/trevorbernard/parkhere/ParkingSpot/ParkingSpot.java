fa3c38d04aa23f9dfcb3062d76ed7cebbab0570d
Trevor Bernard <tsbernar@usc.edu>
2016-10-13 07:03:16 -0700

    adding member variables to ParkingSpot

--- app/src/main/java/com/example/trevorbernard/parkhere/ParkingSpot/ParkingSpot.java
+++ app/src/main/java/com/example/trevorbernard/parkhere/ParkingSpot/ParkingSpot.java
@@ -1,8 +1,29 @@
 package com.example.trevorbernard.parkhere.ParkingSpot;
 
+import android.location.Location;
+import android.media.Image;
+
+import com.example.trevorbernard.parkhere.User.User;
+
+import java.util.ArrayList;
+
 /**
  * Created by trevorbernard on 10/12/16.
  */
 
 public class ParkingSpot {
+
+    private String name;
+    private User owner;
+    private Image image;
+    private String description;
+    private ArrayList<String> reviews;
+    private User occupant;
+    private int price;
+    private boolean isSUV;
+    private boolean isCovered;
+    private Location location;
+    private 
+
+
 }
