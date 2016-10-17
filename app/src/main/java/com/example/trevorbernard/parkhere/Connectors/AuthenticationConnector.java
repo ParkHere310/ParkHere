d1b4a16cd9ee684cb99987845ebd85ce3e8fc074
Jacob Metzman <metzman@usc.edu>
2016-10-16 17:43:12 -0700

    Created authentication connector and spot connector

---
+++ app/src/main/java/com/example/trevorbernard/parkhere/Connectors/AuthenticationConnector.java
@@ -0,0 +1,17 @@
+package com.example.trevorbernard.parkhere.Connectors;
+
+import com.example.trevorbernard.parkhere.User.User;
+
+/**
+ * Created by metzm on 10/16/2016.
+ */
+
+public class AuthenticationConnector {
+    public static boolean register(User user) {
+        return true;
+    }
+
+    public static boolean authenticat(String un, String pass) {
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/trevorbernard/parkhere/Connectors/SpotConnector.java
@@ -0,0 +1,22 @@
+package com.example.trevorbernard.parkhere.Connectors;
+
+import com.example.trevorbernard.parkhere.ParkingSpot.ParkingSpot;
+import com.example.trevorbernard.parkhere.User.User;
+
+/**
+ * Created by metzm on 10/16/2016.
+ */
+
+public class SpotConnector {
+    public static boolean postSpot(ParkingSpot spot, User user) {
+        return true;
+    }
+
+    public static boolean editSpot(ParkingSpot spot, User user) {
+        return true;
+    }
+
+    public static boolean removeSpot(ParkingSpot spot, User user) {
+        return true;
+    }
+}
