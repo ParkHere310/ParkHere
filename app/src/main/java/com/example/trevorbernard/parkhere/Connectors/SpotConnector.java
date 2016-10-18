7fdbceff5d0f0308f283573ab0e66ba9baa9164a
Jacob Metzman <metzman@usc.edu>
2016-10-17 21:23:13 -0700

    finished spotconnector i think

--- app/src/main/java/com/example/trevorbernard/parkhere/Connectors/SpotConnector.java
+++ app/src/main/java/com/example/trevorbernard/parkhere/Connectors/SpotConnector.java
@@ -2,6 +2,8 @@ package com.example.trevorbernard.parkhere.Connectors;
 
 import com.example.trevorbernard.parkhere.ParkingSpot.ParkingSpot;
 import com.example.trevorbernard.parkhere.User.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by metzm on 10/16/2016.
@@ -9,14 +11,20 @@ import com.example.trevorbernard.parkhere.User.User;
 
 public class SpotConnector {
     public static boolean postSpot(ParkingSpot spot, User user) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("ParkingSpots").child(spot.getName()).setValue(spot);
         return true;
     }
 
     public static boolean editSpot(ParkingSpot spot, User user) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("ParkingSpots").child(spot.getName()).setValue(spot);
         return true;
     }
 
     public static boolean removeSpot(ParkingSpot spot, User user) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("ParkingSpots").child(spot.getName()).setValue(null);
         return true;
     }
 }
