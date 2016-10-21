47ec2553c585840effb5d00d6c22177a04d2b50c
Trevor Bernard <tsbernar@usc.edu>
2016-10-21 11:22:59 -0700

    updated constructors for TimeWindow

--- app/src/main/java/com/example/trevorbernard/parkhere/ParkingSpot/TimeWindow.java
+++ app/src/main/java/com/example/trevorbernard/parkhere/ParkingSpot/TimeWindow.java
@@ -1,6 +1,7 @@
 package com.example.trevorbernard.parkhere.ParkingSpot;
 
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -13,13 +14,27 @@ public class TimeWindow {
     Date endDateTime;
 
     // if no start and end date are specified, default constructor
-    // sets end date to now and start date to 1 hour ago
+    // sets end date to now + 15 days (rounded to nearest hour)
+    // and start date to 1 hour before
     public TimeWindow() {
         endDateTime = new Date();
-        startDateTime = new Date(endDateTime.getTime() - 3_600_000L);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(endDateTime);
+        calendar.set(Calendar.MILLISECOND,0);
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MINUTE,0);
+        calendar.add(Calendar.DAY_OF_YEAR,15);
+
+        endDateTime = calendar.getTime();
+        calendar.add(Calendar.HOUR,-1);
+        startDateTime = new Date(endDateTime.getTime());
     }
 
     public TimeWindow(Date start, Date end) {
+        if(start.after(end)) {
+            throw new IllegalArgumentException("In TimeWindow Constructor: " +
+                    "start Date/Time is after end Date/Time");
+        }
         this.startDateTime = start;
         this.endDateTime = end;
     }
