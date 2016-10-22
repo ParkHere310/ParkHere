ad6987bd83a012a5f8ff82d150bdead070c1f705
Zilong Xiao <zilongxi@usc.edu>
2016-10-22 13:09:27 -0700

    test
    test

---
+++ app/src/main/java/com/example/trevorbernard/parkhere/Client/PastReservationActivity.java
@@ -0,0 +1,23 @@
+package com.example.trevorbernard.parkhere.Client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.trevorbernard.parkhere.R;
+
+/**
+ * Created by Hexi on 2016/10/22.
+ */
+
+public class PastReservationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pastReservation);
+        initiateVariables();
+    }
+
+    private void initiateVariables() {
+
+    }
+}
