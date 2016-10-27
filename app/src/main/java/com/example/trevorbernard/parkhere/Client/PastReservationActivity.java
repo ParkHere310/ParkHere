5f659add57cad408bb7c230713bdff3ccafec37d
Hexi Zilong Xiao <zilongxiao@Hexis-MacBook-Air.local>
2016-10-26 22:51:30 -0700

    Past Reservation Activity Updated

--- app/src/main/java/com/example/trevorbernard/parkhere/Client/PastReservationActivity.java
+++ app/src/main/java/com/example/trevorbernard/parkhere/Client/PastReservationActivity.java
@@ -2,8 +2,16 @@ package com.example.trevorbernard.parkhere.Client;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.trevorbernard.parkhere.R;
+
+import java.util.List;
 
 /**
  * Created by Hexi on 2016/10/22.
@@ -16,10 +24,34 @@ public class PastReservationActivity extends Activity {
         setContentView(R.layout.activity_pastreservation);
         initiateVariables();
 
-        
+        populateListView();
+
+        createClickCallback();
     }
 
-    private void initiateVariables() {
+    private void createClickCallback() {
 
+    }
+
+    //ONLY FOR TESTING PURPOSES
+    private void populateListView() {
+        String[] mReservations = {"Reservation Address 1","Reservation Address 2",
+                "Reservation Address3 "};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, R.layout.layout_reservationitem, mReservations);
+        ListView list = (ListView) findViewById(R.id.listView);
+        list.setAdapter(adapter);
+     }
+
+    private void initiateVariables() {
+        ListView list = (ListView) findViewById(R.id.listView);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+                TextView clickedTextView = (TextView) viewClicked;
+                Toast.makeText(PastReservationActivity.this,
+                        "I just clicked on: " + clickedTextView.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
