c3ed1439962a13de8c8bab4fe54bd43ee64b974d
Trevor Bernard <tsbernar@usc.edu>
2016-10-29 16:57:47 -0700

    search result layout:

---
+++ app/src/main/java/com/example/trevorbernard/parkhere/ParkingSpot/ParkingSpotDistance.java
@@ -0,0 +1,10 @@
+package com.example.trevorbernard.parkhere.ParkingSpot;
+
+/**
+ * Created by trevorbernard on 10/29/16.
+ */
+
+public class ParkingSpotDistance {
+    public ParkingSpot parkingSpot;
+    public Double distance;
+}
