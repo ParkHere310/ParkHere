7fcb2c4520c77b593171f1b12c8c36eec118a6b6
Jacob Metzman <metzman@usc.edu>
2016-10-28 17:40:57 -0700

    All filters are written except sort by distance

--- app/src/main/java/com/example/trevorbernard/parkhere/Client/FilterActivity.java
+++ app/src/main/java/com/example/trevorbernard/parkhere/Client/FilterActivity.java
@@ -6,7 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.trevorbernard.parkhere.Connectors.UserConnector;
+import com.example.trevorbernard.parkhere.ParkingSpot.ParkingSpot;
 import com.example.trevorbernard.parkhere.R;
+import com.example.trevorbernard.parkhere.User.User;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by Hexi on 2016/10/23.
@@ -38,5 +45,41 @@ public class FilterActivity extends Activity {
 
     private void initiateVariables() {
         FilterButton = (Button) findViewById(R.id.FilterButton);
+    }
+
+    private ArrayList<ParkingSpot> sortByPrice(ArrayList<ParkingSpot> spotList) {
+        Collections.sort(spotList, new Comparator<ParkingSpot>() {
+            @Override
+            public int compare(ParkingSpot o1, ParkingSpot o2) {
+                return new Integer(o1.getPrice()).compareTo(o2.getPrice());
+            }
+        });
+        return spotList;
+    }
+
+    private ArrayList<ParkingSpot> sortByDistance(ArrayList<ParkingSpot> spotList) {
+        // TODO: this
+    }
+
+    private ArrayList<ParkingSpot> sortBySpotRating(ArrayList<ParkingSpot> spotList) {
+        Collections.sort(spotList, new Comparator<ParkingSpot>() {
+            @Override
+            public int compare(ParkingSpot o1, ParkingSpot o2) {
+                return new Double(o1.getRating().calculateRating()).compareTo(o2.getRating().calculateRating());
+            }
+        });
+        return spotList;
+    }
+
+    private ArrayList<ParkingSpot> sortByOwnerRating(ArrayList<ParkingSpot> spotList) {
+        Collections.sort(spotList, new Comparator<ParkingSpot>() {
+            @Override
+            public int compare(ParkingSpot o1, ParkingSpot o2) {
+                User u1 = UserConnector.getUserFromUID(o1.getOwnerUID());
+                User u2 = UserConnector.getUserFromUID(o2.getOwnerUID());
+                return new Double(u1.getRating().calculateRating()).compareTo(u2.getRating().calculateRating());
+            }
+        });
+        return spotList;
     }
 }
