9777417062c2a5f62af67c026dbe7dc0e8131283
Zilong Xiao <zilongxi@usc.edu>
2016-11-27 23:54:20 -0800

    Typo corrected

--- app/src/main/java/com/example/trevorbernard/parkhere/Client/SearchHeatMapActivity.java
+++ app/src/main/java/com/example/trevorbernard/parkhere/Client/SearchHeatMapActivity.java
@@ -50,7 +50,7 @@ public class SearchHeatMapActivity extends Activity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_searchresult);
+        setContentView(R.layout.activity_searchheatmap);
 
         initiateVariables();
         CreateOnlickCallback();
