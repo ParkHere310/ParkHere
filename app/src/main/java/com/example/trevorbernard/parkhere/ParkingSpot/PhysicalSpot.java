63aca2090c7228119c725328fdf0ba8ace75e162
Trevor Bernard <tsbernar@usc.edu>
2016-11-28 13:42:09 -0800

    fixed PhysicalSpot firebase crashing error

--- app/src/main/java/com/example/trevorbernard/parkhere/ParkingSpot/PhysicalSpot.java
+++ app/src/main/java/com/example/trevorbernard/parkhere/ParkingSpot/PhysicalSpot.java
@@ -29,6 +29,10 @@ public class PhysicalSpot {
 
     private int timesListed = 0;
 
+    public PhysicalSpot(){
+
+    }
+
     public PhysicalSpot(String name,
                        String description,
                        boolean isSUV,
